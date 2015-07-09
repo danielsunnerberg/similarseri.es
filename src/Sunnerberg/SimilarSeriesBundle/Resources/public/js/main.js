886a95a85af35b530122a0609aab7ec775d55fa2
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-07-09 15:50:14 +0200

    Bootstrap3 typeahead with requirejs example

--- src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/main.js
+++ src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/main.js
@@ -4,7 +4,8 @@ var config = {
     },
     paths: {
         'jquery' : ['//code.jquery.com/jquery-2.1.4.min', '/assets/vendor/jquery/dist/jquery'],
-        'bootstrap' : ['/maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min', '/assets/vendor/bootstrap/dist/js/bootstrap.min']
+        'bootstrap' : ['/maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min', '/assets/vendor/bootstrap/dist/js/bootstrap.min'],
+        'bootstrap3-typeahead': '/assets/vendor/bootstrap3-typeahead/bootstrap3-typeahead.min'
     }
 };
 requirejs.config(config);
