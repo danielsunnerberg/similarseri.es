6d1f73c937a1a755a2194c7b39f27ac12ff02d0a
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-07-23 18:55:14 +0200

    Removed usage of MaxCDN since it was sometimes being VERY slow

--- src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/main.js
+++ src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/main.js
@@ -7,8 +7,8 @@ var config = {
         'ripples': { 'deps': ['jquery'] }
     },
     paths: {
-        'jquery' : ['//code.jquery.com/jquery-2.1.4.min', '/assets/vendor/jquery/dist/jquery'],
-        'bootstrap' : ['//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min', '/assets/vendor/bootstrap/dist/js/bootstrap.min'],
+        'jquery' : '/assets/vendor/jquery/dist/jquery',
+        'bootstrap' : '/assets/vendor/bootstrap/dist/js/bootstrap.min',
         'typeahead': '/assets/vendor/typeahead.js/dist/typeahead.jquery',
         'bloodhound': '/assets/vendor/typeahead.js/dist/bloodhound.min',
         'handlebars': '/assets/vendor/handlebars/handlebars.amd.min',
