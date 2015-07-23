badfb29c572aeacdc78a608f3c9a014588f6ba38
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-07-23 19:09:09 +0200

    Site-level initialization extracted

---
+++ src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/common.js
@@ -0,0 +1,11 @@
+/**
+ * Module which initializes javascript which are supposed to run on all pages, such as bootstrap plugins
+ * initialization etc.
+ */
+define(['bootstrap', 'bootstrap-material-design', 'ripples'], function () {
+    return {
+        initialize: function () {
+            $.material.init()
+        }
+    };
+});
--- src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/router.js
+++ src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/router.js
@@ -1,4 +1,4 @@
-define(['backbone', 'underscore', 'views/suggestions', 'views/tvShowSearch'], function (Backbone, _, SuggestionsView, TvShowSearchView) {
+define(['backbone', 'underscore', 'views/suggestions', 'views/tvShowSearch', 'common'], function (Backbone, _, SuggestionsView, TvShowSearchView, Common) {
     var Router = Backbone.Router.extend({
         routes: {
             '*actions': 'defaultRoute' // @todo
@@ -6,7 +6,6 @@ define(['backbone', 'underscore', 'views/suggestions', 'views/tvShowSearch'], fu
     });
 
     var initialize = function () {
-
         var router = new Router;
 
         router.on('route:defaultRoute', function (actions) {
@@ -14,6 +13,8 @@ define(['backbone', 'underscore', 'views/suggestions', 'views/tvShowSearch'], fu
             var events = _.extend({}, Backbone.Events);
             new SuggestionsView({externalEvents: events}).render();
             new TvShowSearchView({events: events}).render();
+
+            Common.initialize();
         });
 
         Backbone.history.start({ pushState:true });
