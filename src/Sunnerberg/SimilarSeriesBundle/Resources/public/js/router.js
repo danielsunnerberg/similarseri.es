9409502faeca90f68a3ed38720fefd01b842a3a4
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-08-09 16:44:02 +0200

    Improvements to the start/login-gui

--- src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/router.js
+++ src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/router.js
@@ -1,22 +1,21 @@
 define(['backbone', 'underscore', 'views/suggestions', 'views/tvShowSearch', 'common'], function (Backbone, _, SuggestionsView, TvShowSearchView, Common) {
     var Router = Backbone.Router.extend({
         routes: {
-            '*actions': 'defaultRoute' // @todo
+            '': 'startRoute',
+            'find': 'findRoute'
         }
     });
 
     var initialize = function () {
         var router = new Router;
 
-        router.on('route:defaultRoute', function (actions) {
-            console.log("No matching route found. Using default.", actions);
+        router.on('route:findRoute', function () {
             var events = _.extend({}, Backbone.Events);
             new SuggestionsView({externalEvents: events}).render();
             new TvShowSearchView({events: events}).render();
-
-            Common.initialize();
         });
 
+        Common.initialize();
         Backbone.history.start({ pushState:true });
     };
 
