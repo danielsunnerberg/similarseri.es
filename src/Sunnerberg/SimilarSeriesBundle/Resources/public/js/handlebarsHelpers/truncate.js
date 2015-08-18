591472cec8666796ed4d4d7692223891b0696d38
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-08-19 00:39:43 +0200

    Fixed bug in Handlebars truncate-helper

--- src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/handlebarsHelpers/truncate.js
+++ src/Sunnerberg/SimilarSeriesBundle/Resources/public/js/handlebarsHelpers/truncate.js
@@ -1,6 +1,6 @@
 require(['handlebars'], function (Handlebars) {
     Handlebars.registerHelper('truncate', function (string, maxLength) {
-        if (string.length > maxLength) {
+        if (string !== null && string.length > maxLength) {
             return string.substr(0, maxLength) + "…";
         }
         return string;
