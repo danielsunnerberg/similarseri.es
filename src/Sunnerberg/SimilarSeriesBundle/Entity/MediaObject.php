e93595c39d3ca9d280c606b14b2533a1cb718fac
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-09-06 16:26:25 +0200

    Introduced entity MediaObject, which stores media using only its path, per TMDB convention, allowing the base URL to be injected when known later.

---
+++ src/Sunnerberg/SimilarSeriesBundle/Entity/MediaObject.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Sunnerberg\SimilarSeriesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MediaObject
+ *
+ * @ORM\Table(name="media_objects")
+ * @ORM\Entity
+ */
+class MediaObject implements \JsonSerializable
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="path", type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @var string
+     */
+    private $baseUrl = "";
+
+    public function __construct($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return MediaObject
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Set base url
+     *
+     * @param $baseUrl
+     * @return $this
+     */
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
+
+        return $this;
+    }
+
+    /**
+     * Gets the url, in the form of "base url + path".
+     *
+     * @return null|string
+     */
+    public function getUrl()
+    {
+        if (empty($this->baseUrl)) {
+            return null;
+        }
+
+        return sprintf('%s%s', $this->baseUrl, $this->getPath());
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->getId(),
+            'path' => $this->getPath(),
+            'url' => $this->getUrl(),
+        ];
+    }
+
+}
---
+++ src/Sunnerberg/SimilarSeriesBundle/Tests/Entity/MediaObjectTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Sunnerberg\SimilarSeriesBundle\Tests\Entity;
+
+use Sunnerberg\SimilarSeriesBundle\Entity\MediaObject;
+
+class MediaObjectTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var MediaObject
+     */
+    private $mediaObject;
+
+    protected function setUp()
+    {
+        $this->mediaObject = new MediaObject('foo.png');
+    }
+
+    public function testPath()
+    {
+        $this->assertEquals('foo.png', $this->mediaObject->getPath());
+        $this->mediaObject->setPath('bar.png');
+        $this->assertEquals('bar.png', $this->mediaObject->getPath());
+    }
+
+    public function testBaseUrl()
+    {
+        $this->assertNull($this->mediaObject->getUrl());
+        $baseUrl = 'https://foo.bar/';
+        $this->mediaObject->setBaseUrl($baseUrl);
+
+        $this->assertEquals($baseUrl . 'foo.png', $this->mediaObject->getUrl());
+
+        $this->mediaObject->setPath('baz.png');
+        $this->assertEquals($baseUrl . 'baz.png', $this->mediaObject->getUrl());
+
+        $otherBaseUrl = 'ftp://crazy.com';
+        $this->mediaObject->setBaseUrl($otherBaseUrl);
+        $this->assertEquals($otherBaseUrl . 'baz.png', $this->mediaObject->getUrl());
+    }
+
+
+    public function testJsonSerializeable()
+    {
+        $this->mediaObject->setPath('foo');
+        $baseUrl = 'http://bar/';
+        $this->mediaObject->setBaseUrl('' . $baseUrl . '');
+
+        $json = json_encode($this->mediaObject);
+        $restored = json_decode($json);
+
+        $this->assertEquals('foo', $restored->path);
+        $this->assertEquals($baseUrl . 'foo', $restored->url);
+    }
+
+}
