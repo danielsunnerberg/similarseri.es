9ca65d639106169d791dbda63809bb6070892e1f
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-09-06 20:40:02 +0200

    Using strict comparsion in validator

--- src/Sunnerberg/SimilarSeriesBundle/Helper/TmdbShowValidator.php
+++ src/Sunnerberg/SimilarSeriesBundle/Helper/TmdbShowValidator.php
@@ -4,21 +4,47 @@ namespace Sunnerberg\SimilarSeriesBundle\Helper;
 
 use Tmdb\Model\Tv;
 
+/**
+ * Provides tools determining whether a show is worth fetching or not.
+ * Typically, TmdbShowValidator#isValid is the function to use.
+ *
+ * Class TmdbShowValidator
+ * @package Sunnerberg\SimilarSeriesBundle\Helper
+ */
 class TmdbShowValidator {
 
+    /**
+     * Checks whether the show is worth fetching by running all available validators on it.
+     *
+     * @param Tv $show
+     * @return bool
+     */
     public function isValid(Tv $show)
     {
         return self::hasValidPoster($show) && self::hasValidOverview($show);
     }
 
-    public static function hasValidPoster(Tv $show)
+    private static function isNullOrEmpty($value)
     {
-        return $show->getPosterPath() != null;
+        return $value === null || empty($value);
     }
 
+    /**
+     * @param Tv $show
+     * @return bool
+     */
+    public static function hasValidPoster(Tv $show)
+    {
+        return ! self::isNullOrEmpty($show->getPosterPath());
+    }
+
+    /**
+     * @param Tv $show
+     * @return bool
+     */
     public static function hasValidOverview(Tv $show)
     {
-        return $show->getOverview() != null;
+        return ! self::isNullOrEmpty($show->getOverview());
     }
 
 }
