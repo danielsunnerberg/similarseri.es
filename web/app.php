a9bbaa0c0f529b49dca3b44a0f2569e1aff83289
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2015-09-13 19:28:13 +0200

    Enabled ApcClassLoader

--- web/app.php
+++ web/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = new ApcClassLoader('sf2', $loader);
+$loader->register(true);
 
 require_once __DIR__.'/../app/AppKernel.php';
 
