fbd4a26a18c89135b51fe6a9183cda69bf2245b2
Daniel Sunnerberg <daniel.sunnerberg@gmail.com>
2016-01-21 22:32:48 +0100

    The similar-to page now displays related titles.

--- src/Sunnerberg/SimilarSeriesBundle/Controller/SimilarToController.php
+++ src/Sunnerberg/SimilarSeriesBundle/Controller/SimilarToController.php
@@ -22,9 +22,19 @@ class SimilarToController extends Controller
             throw $this->createNotFoundException();
         }
 
+        // @todo This injection of base url is insane.
+        // @todo Display actors
+        // @todo Title + SEO
+
         $imageHelper = $this->get('sunnerberg_similar_series.helper.tmdb_image_helper');
         $show->getBackdropImage()->setBaseUrl($imageHelper->getImageBaseUrl(TmdbBackdropSize::W1280));
         $show->getPosterImage()->setBaseUrl($imageHelper->getImageBaseUrl(TmdbPosterSize::W185));
+
+        $similarShows = array_slice($show->getSimilarTvShows()->toArray(), 0, 6);
+        $similarBaseUrl = $imageHelper->getImageBaseUrl(TmdbPosterSize::W342);
+        foreach ($similarShows as $similar) {
+            $similar->getPosterImage()->setBaseUrl($similarBaseUrl);
+        }
 
         return $this->render('SunnerbergSimilarSeriesBundle:SimilarTo:similar-to.html.twig', ['show' => $show]);
     }
